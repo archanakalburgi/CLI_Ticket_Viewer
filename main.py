28933bf96c8f4acdb0bd7d1aad688b1988edca9e
Archana Kalburgi <archanakalburgi@gmail.com>
2021-11-28 08:50:34 -0500

    Imporving the error message and adding more details

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import api
 from tabulate import tabulate
 
 
-class MyCmd(Cmd):
+class ZenDeskCommandLoop(Cmd):
     previous_page = None
     next_page = None
 
@@ -12,7 +12,7 @@ class MyCmd(Cmd):
 
     def do_ticket_detail(self, args):
         """
-        Get ticket details by id
+        Get ticket details by id. type ticket_details <id> hit enter
         """
         parsed_args = args.split(" ")
         if len(parsed_args) == 1:
@@ -23,20 +23,17 @@ class MyCmd(Cmd):
             except Exception as e:
                 print(e)
                 print("Invalid arguments, to see ticket details please provide a ticket like ticket_detail <id> eg: ticket_detail 1")
-                return None
 
     def do_tickets(self, args):
         """
-        Get tickets 10 at a time
+        Get tickets 10 at a time. type: tickets and hit enter
         """
         page_to_show = None
         parsed_args = args.split(" ")  # todo move this to diff function
-        if (
-            len(parsed_args) == 1
-            and parsed_args[0] == "b"
-            and self.previous_page is not None
-        ):
+        if (len(parsed_args) == 1 and parsed_args[0] == "b" and self.previous_page is not None):
             page_to_show = self.previous_page
+        elif parsed_args[0] == "b" and self.previous_page is None:
+            print("No previous page, so getting the 1st page")
         elif self.next_page is not None:
             page_to_show = self.next_page
         else:
@@ -52,7 +49,6 @@ class MyCmd(Cmd):
                 self._print_tickets_table(tickets)
         except Exception as e:
             print(e)
-            return None
 
     def do_exit(self, args):
         """
@@ -66,9 +62,9 @@ class MyCmd(Cmd):
             "subject",
             "status",
             "priority",
-            "requester_id",
-            "assignee_id",
-            "organization_id",
+            "requester",
+            "assignee",
+            "organization",
             "tags",
             "created_at",
             "ticket_type",
@@ -82,17 +78,20 @@ class MyCmd(Cmd):
             print(f"Failed to get meta data, this is not fatal. Will try again on next command")
             return None
 
-welcome_string= """ Welcome to Zendesk CLI. 
+
+welcome_string= """ 
+Welcome to Zendesk CLI. 
 Available commands:
 -------------------------------------------------------
 tickets -            to get tickets 10 at a time
 ticket_detail <id> - to get ticket details by id
 -------------------------------------------------------
 Note: 
-1. If user id and organization endpoint is not available, you will see an ID. We try to get that information on every tickets request
-2. Application State is not persisted.
+1. If you press enter without typing any command, it will show you the next 10 tickets.or previous 10 tickets depending on what was the last command you ran.
+2. If user id and organization endpoint is not available, you will see an ID. We try to get that information on every tickets request
+3. Application State is not persisted.
 """
 if __name__ == "__main__":
 
-    app = MyCmd()
+    app = ZenDeskCommandLoop()
     app.cmdloop(welcome_string)
