746e5479226ebf9c10dfac30143003cbd19e90a0
Archana Kalburgi <archanakalburgi@gmail.com>
2021-11-27 20:59:38 -0500

    Adding tests

--- test_api_call.py
+++ test_api_call.py
@@ -1,9 +1,52 @@
 import api_call
+import pytest
+import requests
+import responses
+from requests.exceptions import ConnectionError, HTTPError
+import test_data
+import json
 
-def test_get_current_page_num():
-    page = api_call.get_current_page_num("invalid uri")
-    assert  page == 1
 
 def test_invalid_uri():
-    page = api_call.call_zendesk_api("http://invalid-uri.com")
-    assert  page == 1 
+    pytest.raises(Exception, api_call.get_current_page_num, "invalid uri")
+
+
+@responses.activate
+def test_404_should_not_kill_2():
+    responses.add(
+        responses.GET, "https://zccarchanazendeskcom.zendesk.com/", status=500
+    )
+    pytest.raises(
+        HTTPError,
+        api_call.call_zendesk_api,
+        "https://zccarchanazendeskcom.zendesk.com/",
+    )
+
+
+@responses.activate
+def test_should_parse_valid_json():
+    mock_uri = "https://zccarchanazendeskcom.zendesk.com/"
+    f = open("test_data/test.json", "r")
+    responses.add(
+        responses.GET,
+        mock_uri,
+        status=200,
+        json=json.load(f),
+        content_type="application/json",
+    )
+    resp = api_call.get_tickets(mock_uri)
+    len(resp) == 10
+
+
+@responses.activate
+def test_invalid_json_should_not_kill_app():
+    mock_uri = "https://zccarchanazendeskcom.zendesk.com/"
+    f = open("test_data/test_invalid.json", "r")
+    responses.add(
+        responses.GET,
+        mock_uri,
+        status=200,
+        json=json.load(f),
+        content_type="application/json",
+    )
+    pytest.raises(KeyError, api_call.get_tickets, mock_uri)
