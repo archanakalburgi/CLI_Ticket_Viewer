e54b113f448773cc45687b70acfa0a6a1cf3f4d1
Archana Kalburgi <archanakalburgi@gmail.com>
2021-11-28 07:21:31 -0500

    Adding test folder

---
+++ test/test_api_call.py
@@ -0,0 +1,50 @@
+import api_call
+import pytest
+import responses
+from requests.exceptions import HTTPError
+import json
+
+
+def test_invalid_uri():
+    pytest.raises(Exception, api_call.get_current_page_num, "invalid uri")
+
+
+@responses.activate
+def test_404_should_not_kill_2():
+    responses.add(
+        responses.GET, "https://zccarchanazendeskcom.zendesk.com/", status=500
+    )
+    pytest.raises(
+        HTTPError,
+        api_call.call_zendesk_api,
+        "https://zccarchanazendeskcom.zendesk.com/",
+    )
+
+
+@responses.activate
+def test_should_parse_valid_json():
+    mock_uri = "https://zccarchanazendeskcom.zendesk.com/"
+    f = open("test_data/test.json", "r")
+    responses.add(
+        responses.GET,
+        mock_uri,
+        status=200,
+        json=json.load(f),
+        content_type="application/json",
+    )
+    resp = api_call.get_tickets(mock_uri)
+    len(resp) == 10
+
+
+@responses.activate
+def test_invalid_json_should_not_kill_app():
+    mock_uri = "https://zccarchanazendeskcom.zendesk.com/"
+    f = open("test_data/test_invalid.json", "r")
+    responses.add(
+        responses.GET,
+        mock_uri,
+        status=200,
+        json=json.load(f),
+        content_type="application/json",
+    )
+    pytest.raises(KeyError, api_call.get_tickets, mock_uri)
